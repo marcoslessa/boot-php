428259915025b4c46cc7b60c9e7ebc2352debfd1
Marcos Lessa <mvr.lessa@gmail.com>
2021-09-30 00:25:42 -0300

    refactor: modify controllers system, add some helpers

--- views/index.view.php
+++ views/index.view.php
@@ -1,17 +1,5 @@
 <?php require('partials/head.php'); ?>
 
-<ul>
-<?php foreach ($users as $user): ?>
-    <li><?=$user->name?></li>
-<?php endforeach; ?>
-</ul>
-
-<h1>Submit Your Name</h1>
-
-<form action="/names" method="post">
-    <input type="text" name="name"></input>
-    <button type="submit">Enviar</button>
-</form>
-
+    <h1>Home page</h1>
 
 <?php require('partials/footer.php'); ?>
--- views/partials/head.php
+++ views/partials/head.php
@@ -2,7 +2,7 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <title>About our culture</title>
+    <title>Boot PHP</title>
     <link rel="stylesheet" href="/public/css/bootstrap.min.css">
 </head>
 <body>
